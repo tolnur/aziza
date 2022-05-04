9f40d85c6fbb705d72b1461613751020bc895f2d
azizannnnnn <104884269+azizannnnnn@users.noreply.github.com>
2022-05-04 11:36:51 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>METANIT.COM</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Введите свои данные:</h1>
+<form action="display.php" method="POST">
+    <p>Введите имя: <input type="text" name="firstname" /></p>
+    <p>Введите фамилию: <input type="text" name="lastname" /></p>
+    <input type="email" name="email" placeholder="Введите Email"
+           class="form-control"><br>
+    <textarea name="message" class="form-control"
+              class="mb-30" placeholder="Введите ваше сообщение"></textarea><br>
+    <button type="submit" name="send" class="btn btn-success">Отправить</button>
+</form>
+</body>
+</html>
